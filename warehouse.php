dff6f96836e3cc5de1882d8ab7011c3fb10973b7
Bashe <ibnubaashe999@gmail.com>
2026-04-30 10:42:39 +0300

    Update warehouse.php

--- warehouse.php
+++ warehouse.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_warehouse'])) {
 <div class="page-header">
     <h1>Warehouse & Suppliers</h1>
 </div>
-
+ 
 <div class="flex-row">
     <div class="flex-col glass" style="padding: 2rem;"> 
         <h3 style="margin-bottom: 1.5rem; color: var(--primary-color);">Receive New Stock</h3>
