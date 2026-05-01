bc8140cda20d0cb1dc7b50415ba4c636dc569524
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-01 09:59:27 +0300

    Update chat.php

--- chat.php
+++ chat.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['send_msg'])) {
     $stmt = $conn->prepare("INSERT INTO messages (sender, message) VALUES (?, ?)");
     $stmt->execute([$user, $msg]);
     header("Location: chat.php");
-    exit();
+    exit(); 
 }
 ?> 
 <style> 
