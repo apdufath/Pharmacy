fe7e28eca48c49d92b739532bafab1ebd27a8551
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-02 14:18:44 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $pass = '';
 $dbname = 'pharmacy_db'; 
  
 try { 
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass); 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 } catch(PDOException $e) {
     if ($e->getCode() == 1049) {
