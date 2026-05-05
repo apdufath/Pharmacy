1a835779d8ac2545ad5a04c26eba01af14bb140b
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-05 10:01:31 +0300

    Update warehouse.php

--- warehouse.php
+++ warehouse.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_warehouse'])) {
     exit();  
 }    
 ?> 
- 
+  
 <div class="page-header">
     <h1>Warehouse & Suppliers</h1> 
 </div>
