522113e5ac319ee77241f653aad4444c1608955e
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-07 18:40:14 +0300

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
 } 
- 
+  
 $error = ''; 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username']; 
