b370698a1e695759a25a987fb8ddccbb31c7077b
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-07 09:24:34 +0300

    Fix formatting of category assignment in form handling

--- edit_medicine.php
+++ edit_medicine.php
@@ -8,7 +8,7 @@ if (!$id) {
 // Handle form submission
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['edit_medicine'])) {
     $name = $_POST['name'];
-    $category = $_POST['category']; 
+    $category = $_POST['category'];
     $price = $_POST['price'];
     $stock = $_POST['stock'];
     $expiry = $_POST['expiry_date'];  
