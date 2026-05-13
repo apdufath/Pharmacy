f1a516737026c5078634902018a7a3f0cc391b6b
Abdifatah Bashe <ibnubaashe999@gmail.com>
2026-05-13 11:21:56 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'pharmacy_db'; 
-      
+       
 try { 
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass); 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
